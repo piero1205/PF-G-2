53a03a0152d7992b18e8ab9c403e31c96e72611b
piero1205 <pjcarva12@gmail.com>
2025-11-24 14:46:53 -0600

    Update Mazo.java

--- ProyectoED/src/Mazo.java
+++ ProyectoED/src/Mazo.java
@@ -1,1 +1,56 @@
+import java.util.Stack;
+import java.util.Collections;
+import java.util.List;
 
+public class Mazo {
+
+    private Stack<Carta> pila;
+
+    public Mazo() {
+        pila = new Stack<>();
+    }
+
+    public void agregarCartas(List<Carta> cartas) {
+        Collections.shuffle(cartas);
+        pila.addAll(cartas);
+    }
+
+    public void push(Carta carta) {
+        pila.push(carta);
+    }
+
+    public Carta pop() {
+        if (pila.isEmpty()) {
+            return null;
+        }
+        return pila.pop();
+    }
+
+    public Carta peek() {
+        if (pila.isEmpty()) {
+            return null;
+        }
+        return pila.peek();
+    }
+
+    public boolean estaVacio() {
+        return pila.isEmpty();
+    }
+
+    public int tamaño() {
+        return pila.size();
+    }
+
+    public void mostrarMazo() {
+        System.out.println("=== Contenido Mazo ===");
+        for (Carta c : pila) {
+            System.out.println(c.getValor() + " de " + c.getPalo());
+        }
+        System.out.println("==========================");
+    }
+
+
+    public Stack<Carta> getPila() {
+        return pila;
+    }
+}
