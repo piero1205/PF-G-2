4b84974a6545fe7343b9e3dd9639d979837e8e3e
piero1205 <pjcarva12@gmail.com>
2025-11-24 17:35:23 -0600

    Implement Pozo class for card management

--- ProyectoED/src/Pozo.java
+++ ProyectoED/src/Pozo.java
@@ -1,1 +1,54 @@
+import java.util.Stack;
+import java.util.List;
+import java.util.ArrayList;
 
+public class Pozo {
+
+    private Stack<Carta> pilaPozo;
+
+    public Pozo() {
+        pilaPozo = new Stack<>();
+    }
+
+    public void tirarCarta(Carta carta) {
+        pilaPozo.push(carta);
+    }
+
+    public Carta verUltimaCarta() {
+        if (pilaPozo.isEmpty())
+            return null;
+
+        return pilaPozo.peek();
+    }
+
+
+    public Carta sacarUltimaCarta() {
+        if (pilaPozo.isEmpty())
+            return null;
+
+        return pilaPozo.pop();
+    }
+
+
+    public int getTamaño() {
+        return pilaPozo.size();
+    }
+
+    public boolean estaVacio() {
+        return pilaPozo.isEmpty();
+    }
+
+
+    public void mostrarPozo() {
+        System.out.println(" Cartas en el Pozo ");
+        for (Carta c : pilaPozo) {
+            System.out.println(c.getValor() + " de " + c.getPalo());
+        }
+        System.out.println("--------------------");
+    }
+
+
+    public List<Carta> obtenerComoLista() {
+        return new ArrayList<>(pilaPozo);
+    }
+}
